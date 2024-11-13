e31035c0021c68ec1f351a275cc938566189ff21
Fernando Yaguana <109767537+FernandoYaguana@users.noreply.github.com>
2024-11-12 21:31:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +11,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
